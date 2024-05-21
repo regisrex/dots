8059a7e309bb9a5fca8ed1f22c4e69bc3d79e645
Régis N <ndizihiweregis06@gmail.com>
2024-05-21 23:10:00 +0200

    Create server-setup.sh

---
+++ server-setup.sh
@@ -0,0 +1,14 @@
+# Install node using nvm
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  source ~/.bashrc
+
+  nvm install 20.13.1
+
+# Install turbo and pm2
+npm i -g turbo
+npm i -g pm2
+
+
+# Install ngnix
+sudo apt update  && sudo apt upgrade
+sudo apt install nginx
